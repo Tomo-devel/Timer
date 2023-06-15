56de57ed5032cbb5a0c12947233185139bc91910
Tomo-dev <124762208+tomo-devl@users.noreply.github.com>
2023-06-15 13:26:45 +0900

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by 井上智輝 on 2023-06-15.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
